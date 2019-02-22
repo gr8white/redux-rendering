ebf612132da7bba74a878fc02dce63b0fa122a7c
Derrick White <gr8white@uga.edu>
2019-02-22 11:16:00 -0600

    finished rectangle

--- rectangle/index.js
+++ rectangle/index.js
@@ -3,7 +3,18 @@ const { subscribe, dispatch, getState } = createStore(reducer);
 
 subscribe(() => render(getState()));
 
-dispatch({type: null}); // Here we're making a call to dispatch() - this triggers the first render.
+// dispatch({type: null}); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+var width = document.getElementById("width")
+width.addEventListener('click', e => dispatch({type: "WIDTH"}), console.log("index"))
+
+var height = document.getElementById("height")
+height.addEventListener('click', e => dispatch({type: "HEIGHT"}), console.log("index"))
+
+var red = document.getElementById("red")
+red.addEventListener('click', e => dispatch({type: "COLOR", color: "red"}), console.log("index"))
+
+var blue = document.getElementById("blue")
+blue.addEventListener('click', e => dispatch({type: "COLOR",color: "blue"}), console.log("index"))
--- rectangle/reducer.js
+++ rectangle/reducer.js
@@ -1,18 +1,45 @@
 const initialState = {
-    width: 100,
-    height: 200,
-    color: "#FF00FF"
+  width: 100,
+  height: 200,
+  color: "#FF00FF"
 };
 
 const reducer = (state = initialState, action) => {
-    // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+  // Handle actions here - make sure you don't mutate the state!
+  const { type, color } = action;
 
-    // ACTION 1 - Increment height by 10
+  // ACTION 1 - Increment height by 10
+  if (type === "HEIGHT") {
+    console.log("reducer")
+    var newObject =  {
+      ...state,
+      height: state.height + 10
+    }
+  }
+  // ACTION 2 - Increment width by 10
+  if (type === "WIDTH") {
+    console.log("reducer")
+    var newObject =  {
+      ...state,
+      width: state.width + 10
+    }
+  }
+  // ACTION 3 - Change the color
+  if (type === "COLOR") {
+    if (color === "red") {
+      console.log("reducer")
+      var newObject =  {
+        ...state,
+        color: "#ff0000"
+      }
+    } else if (color == "blue") {
+      console.log("reducer")
+      var newObject =  {
+        ...state,
+        color: "#0000ff"
+      }
+    }
+  }
 
-    // ACTION 2 - Increment width by 10
-
-    // ACTION 3 - Change the color
-
-    return state;
+  return newObject;
 }
