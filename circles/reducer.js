c2f05f73ccf017b9066fdc5b0de89800f325623a
Derrick White <gr8white@uga.edu>
2019-02-21 18:38:48 -0600

    finished circles

--- circles/reducer.js
+++ circles/reducer.js
@@ -1,27 +1,49 @@
 const initialState = [
-    {
-        radius: 50,
-        color: "#FF00FF"
-    },
-    {
-        radius: 30,
-        color: "#FF99AA"
-    },
-    {
-        radius: 60,
-        color: "#0000FF"
-    },
-    {
-        radius: 10,
-        color: "#000000"
-    },
+  {
+    radius: 50,
+    color: "#FF00FF"
+  },
+  {
+    radius: 30,
+    color: "#FF99AA"
+  },
+  {
+    radius: 60,
+    color: "#0000FF"
+  },
+  {
+    radius: 10,
+    color: "#000000"
+  },
 ];
 
+function getRandomNumber(max) {
+  return Number(Math.floor(Math.random() * Math.floor(max)))
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const reducer = (state = initialState, action) => {
-    // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+  // Handle actions here - make sure you don't mutate the state!
+  let { type } = action;
 
-    // ACTION: Add a random circle
-
-    return state;
+  // ACTION: Add a random circle
+  if (type === "NEW-CIRCLE") {
+  console.log("new-circle")  
+    var newState = [
+      ...state,
+      {
+        radius: getRandomNumber(100),
+        color: getRandomColor()
+      }
+    ]
+  }
+  return newState;
 }
--- circles/render.js
+++ circles/render.js
@@ -1,15 +1,15 @@
-const render = (state) => {
-    const circlesDOM = document.getElementById('circlesContainer');
+const render = (newState) => {
+  const circlesDOM = document.getElementById('circlesContainer');
 
-    var circlesHTML = state.map(function (circle) {
-        return `
-            <div style="width: ${circle.radius * 2}px; height: ${circle.radius * 2}px; border-radius: ${circle.radius}px; background-color: ${circle.color};"></div>
-        `
-    });
-
-    circlesDOM.innerHTML = `
-        <div class="d-flex flex-column align-items-center">
-            ${circlesHTML.join('')}
-        </div>
+  var circlesHTML = newState.map(function (circle) {
+    return `
+      <div style="width: ${circle.radius * 2}px; height: ${circle.radius * 2}px; border-radius: ${circle.radius}px; background-color: ${circle.color};"></div>
     `
+  });
+
+  circlesDOM.innerHTML = `
+    <div class="d-flex flex-column align-items-center">
+      ${circlesHTML.join('')}
+    </div>
+  `
 }
